4def52a3d860adb8dab6387bcf8f716c404e46e0
David Swords <95060239+davidswords@users.noreply.github.com>
2024-05-19 11:27:36 +0200

    Updated the setup.py, split out the requirements. Added pyenv versin file.

--- setup.py
+++ setup.py
@@ -1,42 +1,16 @@
 from setuptools import setup, find_packages
 
+def read_requirements(file_name):
+    with open(file_name) as f:
+        return f.read().splitlines()
+
 setup(
     name="clean-alchemy",
-    version="0.1.12",
+    version="0.1.13",
     packages=find_packages(exclude=["tests*", "build*"]),
-    install_requires=[
-        "annotated-types==0.6.0",
-        "black==24.4.2",
-        "click==8.1.7",
-        "exceptiongroup==1.2.1",
-        "factory-boy==3.3.0",
-        "Faker==25.0.1",
-        "iniconfig==2.0.0",
-        "mypy-extensions==1.0.0",
-        "packaging==24.0",
-        "pathspec==0.12.1",
-        "platformdirs==4.2.1",
-        "pluggy==1.5.0",
-        "psycopg2==2.9.9",
-        "pydantic==2.7.1",
-        "pydantic_core==2.18.2",
-        "pytest==8.2.0",
-        "pytest-mock==3.14.0",
-        "pytest-sugar==1.0.0",
-        "python-dateutil==2.9.0.post0",
-        "six==1.16.0",
-        "SQLAlchemy==2.0.30",
-        "termcolor==2.4.0",
-        "tomli==2.0.1",
-        "typing_extensions==4.11.0",
-    ],
+    install_requires=read_requirements("requirements.txt"),
     extras_require={
-        "dev": [
-            "pytest==8.2.0",
-            "pytest-mock==3.14.0",
-            "factory-boy==3.3.0",
-            "pytest-sugar==1.0.0",
-        ],
+        "dev": read_requirements("requirements-dev.txt"),
     },
     author="David Swords",
     author_email="furuer_svette.0k@icloud.com",
@@ -50,5 +24,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9.6",
+    python_requires=">=3.12.3",
 )
